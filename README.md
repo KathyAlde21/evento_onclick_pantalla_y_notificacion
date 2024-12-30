03d8cfdba8344e788727a9aedaa97a96d653eda9
KathyAlde21 <katherinealdereteg@gmail.com>
2024-12-29 23:18:15 -0300

    checkbox

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 **_<h1 align="center">:vulcan_salute: Ejercicio Clase Eventos OnClick :computer:</h1>_**
 
-**<h4>Evento OnClick</h4>**
+**<h2>Evento OnClick</h2>**
 Proyecto en que se van a crear eventos asociados a botones y cambio en fuentes, con la creación de su respectiva carpeta
 
 Utilizando dos botones se muestran dos formas diferentes de llamar un mensaje, una en pantalla y otra como emergente:
@@ -10,16 +10,20 @@ Utilizando dos botones se muestran dos formas diferentes de llamar un mensaje, u
 - emergente:
 <img src="./app/src/main/res/drawable/texto_notificacion.jpg" alt="">
 
-Puedo llamar en ambas al mismo elemento o pueden ser diferentes, en este caso llame al mismo:
+- Puedo llamar en ambas al mismo elemento o pueden ser diferentes, en este caso llame al mismo:
 <img src="./app/src/main/res/drawable/notificacion_mostrar_iguales.jpg" alt="">
 
-**<h4>Cambio de Fuente</h4>**
-Descargo desde la página que yo elija, en este caso google fonts y creo la carpeta donde se va a almacenar. Click derecho en res => New => Android Resource Directory => font
+
+**<h2>Cambio de Fuente</h2>**
+- Descargo desde la página que yo elija, en este caso google fonts y creo la carpeta donde se va a almacenar. Click derecho en res => New => Android Resource Directory => font
+- Guardo la fuente descargada en la carpeta recién creada y la llamo:
+<img src="./app/src/main/res/drawable/cargando_fuente.jpg" alt="">
+
+- Puedo editarla cambiando color, estilo u otros, en este caso aumente su tamaño y coloque negrita:
+<img src="./app/src/main/res/drawable/editando_fuente.jpg" alt="">
+
+**<h2>Botones CheckBox</h2>**
+
 <img src="./app/src/main/res/drawable/ejercicio_clase..." alt="">
-
-Guardo la fuente descargada en la carpeta recién creada y la llamo
-
-
-
-
+<img src="./app/src/main/res/drawable/ejercicio_clase..." alt="">
 <img src="./app/src/main/res/drawable/ejercicio_clase..." alt="">
--- app/src/main/java/cl/bootcamp/claseonclick/MainActivity.java
+++ app/src/main/java/cl/bootcamp/claseonclick/MainActivity.java
@@ -3,6 +3,7 @@ package cl.bootcamp.claseonclick;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,11 +14,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    //aca declaro las variables y para eso llamo lo mismo que cree, ej: TexView tal cosa, etc
+    //aca declaro todas las variables y para eso llamo lo mismo que cree, ej: TexView tal cosa, etc
     //debo importarlas una vez que las creo con click derecho
     TextView mostrar;
     EditText texto;
-    Button btnMostrar, btnNotificacion;
+    Button btnMostrar, btnNotificacion, btnValidarCb;
+    CheckBox cbOpcion1, cbOpcion2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +27,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main); //lee completo asi que puedo llamar directo el id
 
 
-    //llamando variables => carpeta res representada en R y luego id porque ya estoy en activity_main
+    //llamando todas las variables => carpeta res representada en R y luego id porque ya estoy en activity_main
         texto = (EditText) findViewById(R.id.etMostrar);
         mostrar = (TextView) findViewById(R.id.tvMotrar);
         btnMostrar = (Button) findViewById(R.id.btnMostrar);
         btnNotificacion = (Button) findViewById(R.id.btnNotificacion);
+        //variables checkbox y su boton
+        btnValidarCb = (Button) findViewById(R.id.btnValidarCb);
+        cbOpcion1 = (CheckBox) findViewById(R.id.cbOpcion1);
+        cbOpcion2 = (CheckBox) findViewById(R.id.cbOpcion2);
 
     //evento onclick btnMostrar
         btnMostrar.setOnClickListener(new View.OnClickListener() {
@@ -39,7 +45,10 @@ public class MainActivity extends AppCompatActivity {
                 mostrar.setText(texto.getText());
             }
         });
+        //mostrar texto btn mostrar y notificcion
         mostrarNotificacion();
+        //validando checkbox
+        validarCheckBox();
     }
 
     //evento onclick btnNotificacion llamando a mostrarNotificacion
@@ -53,5 +62,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    //evento onclick checkbox
+     public void validarCheckBox() {
+        btnValidarCb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //para saber que boton valide
+                String cadena = "Seleccionado: ";
+                if (cbOpcion1.isChecked()) {
+                    cadena += cbOpcion1.getText();
+                }
+                if (cbOpcion2.isChecked()) {
+                    cadena += cbOpcion2.getText();
+                }
+                if (!cbOpcion1.isChecked() && !cbOpcion2.isChecked()) {
+                    cadena += "No ha seleccionado ninguna opción";
+                }
+                Toast.makeText(MainActivity.this, cadena, Toast.LENGTH_LONG).show();
+            }
+        });
+     }
 
 }
